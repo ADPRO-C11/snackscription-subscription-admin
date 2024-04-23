105456bbcea5393c019e4556c1b1653e623b7ee4
sdikyarts <wtrcrss.family@gmail.com>
2024-04-23 23:03:02 +0700

    Initial commit

---
+++ src/main/java/snackscription/subscriptionadmin/SubscriptionAdminApplication.java
@@ -0,0 +1,13 @@
+package snackscription.subscriptionadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/snackscription/subscriptionadmin/SubscriptionAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package snackscription.subscriptionadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
