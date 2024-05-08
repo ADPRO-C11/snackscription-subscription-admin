788f1d6d3555688e836c6c32b5e97549bf6bde1f
sdikyarts <wtrcrss.family@gmail.com>
2024-05-08 11:05:51 +0700

    Add Factory Skeleton

---
+++ src/main/java/snackscription/subscriptionadmin/factory/AdminRepositoryFactory.java
@@ -0,0 +1,10 @@
+package snackscription.subscriptionadmin.factory;
+
+import snackscription.subscriptionadmin.repository.AdminRepository;
+
+public class AdminRepositoryFactory implements Factory<AdminRepository> {
+    @Override
+    public AdminRepository create() {
+        return null;
+    }
+}
---
+++ src/main/java/snackscription/subscriptionadmin/factory/AdminSubscriptionFactory.java
@@ -0,0 +1,10 @@
+package snackscription.subscriptionadmin.factory;
+
+import snackscription.subscriptionadmin.model.AdminSubscription;
+
+public class AdminSubscriptionFactory implements Factory<AdminSubscription> {
+    @Override
+    public AdminSubscription create() {
+        return null;
+    }
+}
